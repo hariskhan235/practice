ae205475fdf1dc715c6156c44464ee24dfd93fcb
Muhammad Haris Akhtar <muhammadharis9603@gmail.com>
2023-09-29 15:41:33 +0500

    decrement function implemented

--- lib/main.dart
+++ lib/main.dart
@@ -92,6 +92,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
