1ce370bf138195ebe7fe3de60961295ca1aa81ac
Muhammad Haris Akhtar <muhammadharis9603@gmail.com>
2023-09-30 22:40:57 +0500

    in new branch

---
+++ lib/new_for_my_new_branch.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class NewBranch extends StatefulWidget {
+  const NewBranch({Key? key}) : super(key: key);
+
+  @override
+  State<NewBranch> createState() => _NewBranchState();
+}
+
+class _NewBranchState extends State<NewBranch> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
